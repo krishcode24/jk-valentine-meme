727f05a2a5d781359ea199b75f2f8032fb0a7ff1
Krishna Selvakone <krishna242004@gmail.com>
2026-02-02 16:02:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -54,15 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
+    "Are you sure paa?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Paa please...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
+    "If you say no, I will be really sad paa...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
+    "Hmph..hmm",
     "Just kidding, say yes please! ❤️"
 ];
 
